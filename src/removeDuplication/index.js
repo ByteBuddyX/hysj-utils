f3c97d288ea14415de689e37adc033b97725565e
huyansj <1296680359@qq.com>
2022-04-08 15:08:44 +0800

    添加md文件

--- src/removeDuplication/index.js
+++ src/removeDuplication/index.js
@@ -1,9 +1,9 @@
 export const basicsRemoveDuplication = (array) => Array.from(new Set(array))
-export const complexRemoveDuplication = (array, id) => {
+export const complexRemoveDuplication = (array, key) => {
   const map = new Map()
   return array.reduce((cur, next) => {
-    if (!map.has(next[id])) {
-      map.set(next[id], true)
+    if (!map.has(next[key])) {
+      map.set(next[key], true)
       cur.push(next)
     }
     return cur
